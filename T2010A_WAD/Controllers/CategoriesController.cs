6c73def904f6c98af2d3455ba7a9a5e21fa5f65b
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-11-26 15:29:01 +0700

    update entity

--- T2010A_WAD/Controllers/CategoriesController.cs
+++ T2010A_WAD/Controllers/CategoriesController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using T2010A_WAD.Models;
 
 namespace T2010A_WAD.Controllers
 {
     public class CategoriesController : Controller
     {
+        private DataContext context = new DataContext();
         // GET: Categories
         public ActionResult Index()
         {
-            return View();
+            ViewData["Page-Title"] = "Categories Page";
+            ViewBag.PageTitle = "Demo Title for Categories Page";
+            // var category = new Category() {Id=1,CategoryNam e="Fashion" };
+            // ViewBag.Category = category;
+            var list = context.Categories.ToList();
+       
+            return View(list); // passing data by model
         }
 
         public ActionResult Create()
---
+++ T2010A_WAD/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace T2010A_WAD.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
