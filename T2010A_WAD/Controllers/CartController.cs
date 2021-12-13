75387357fb151f32d47e51156ad0e9e2ad91ed75
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-12-13 15:10:06 +0700

    session

---
+++ T2010A_WAD/Controllers/CartController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using T2010A_WAD.Models;
+namespace T2010A_WAD.Controllers
+{
+    public class CartController : Controller
+    {
+        private DataContext db = new DataContext();
+        
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddToCart(int? id,int? qty)
+        {
+            var product = db.Products.Find(id);
+            if(product != null)
+            {
+                CartItem item = new CartItem() {Product=product,Qty=(int)qty };// gia sử thêm 1 sp với qty là 1
+                Cart cart = Session["Cart"] as Cart;
+                if(cart == null) // khi ma chưa có sp nào trong gio hàng
+                {
+                    cart = new Cart() { GrandTotal = 0, Items = new List<CartItem>() };
+                }
+                cart.AddToCart(item);
+                Session["Cart"] = cart; // thêm lại vào session
+                return RedirectToAction("Index");
+            }
+            return HttpNotFound();
+        }
+    }
+}
---
+++ T2010A_WAD/Models/Cart.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T2010A_WAD.Models
+{
+    public class Cart
+    {
+        public int GrandTotal { get; set; }
+        public List<CartItem> Items { get; set; }
+
+        public void CalculateGrandTotal()
+        {
+            int grand = 0;
+            foreach(var i in Items)
+            {
+                grand += i.Qty * i.Product.Price;
+            }
+            GrandTotal = grand;
+        }
+
+        public void AddToCart(CartItem item)
+        {
+            // kiểm tra xem sp đã có trong giỏ hàng chưa, nếu có rồi chỉ thêm số lượng
+            // nếu chưa có mới thêm nguyên item vào giỏ hàng
+            bool flag = false;
+            foreach(var i in Items)
+            {
+                if(item.Product.Id == i.Product.Id)
+                {
+                    flag = true;
+                    i.Qty += item.Qty;
+                }
+            }
+            if (!flag)
+            {
+                Items.Add(item);
+            }
+            CalculateGrandTotal();
+        }
+
+    }
+
+    public class CartItem
+    {
+        public Product Product { get; set; }
+        public int Qty { get; set; }
+    }
+}
